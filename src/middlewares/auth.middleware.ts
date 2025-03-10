80e19614b54cd993325d550f8b4b539a6030ae06
HXRRY <harrydng1209@gmail.com>
2025-03-10 17:30:15 +0700

    [TICKET-032]: Remove default title fallback in auth middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -8,7 +8,7 @@ export default defineNuxtRouteMiddleware(async (to) => {
   const authStore = useAuthStore();
 
   useHead({
-    title: String(to.meta.title) || 'Code Base Nuxt 3',
+    title: String(to.meta.title),
   });
 
   if (to.meta.requiresAuth) {
