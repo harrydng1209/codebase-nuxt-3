73f28b39f8c38b7d4813daeae3f0c98df3024ad7
HXRRY <harrydng1209@gmail.com>
2025-05-06 10:32:11 +0700

    [TICKET-054]: Add vue-query and update lint-staged version

--- src/plugins/vue-tippy.plugin.ts
+++ src/plugins/vue-tippy.plugin.ts
@@ -1,17 +1,17 @@
-import vueTippy from 'vue-tippy';
-
-const options = {
-  component: 'tippy',
-  componentSingleton: 'tippy-singleton',
-  defaultProps: {
-    allowHTML: true,
-    duration: [200, 200],
-    placement: 'top',
-    theme: 'material',
-  },
-  directive: 'tippy',
-};
+import vueTippy, { type TippyPluginOptions } from 'vue-tippy';
 
 export default defineNuxtPlugin((nuxtApp) => {
+  const options: TippyPluginOptions = {
+    component: 'tippy',
+    componentSingleton: 'tippy-singleton',
+    defaultProps: {
+      allowHTML: true,
+      duration: [200, 200],
+      placement: 'top',
+      theme: 'material',
+    },
+    directive: 'tippy',
+  };
+
   nuxtApp.vueApp.use(vueTippy, options);
 });
